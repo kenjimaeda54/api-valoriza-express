0d014a3bbbb1fb308808a2618967da1136b830bc
Ricardo kenji Vivas Maeda <ricardo.maeda@veget.com.br>
2021-12-03 13:18:07 -0300

    feature: finished entitidade user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,9 +1,43 @@
-import { Entity } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
 //para criar entidades no banco de dados
 // yarn  typeorm entity:create -n User
 //para remover a migration do banco de dados
 // yarn typeorm migration:revert
 @Entity()
-class User {}
+class User {
+  @PrimaryColumn()
+  //permito outras classes nao modificarem o id
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    //vou verificar se a minha classe tem um id, se não tiver ele vai criar um id
+    //constructor acesso a classe user e vai verificar se tem um id, se não tiver ele vai criar um id
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
 
 export { User };
